69dbfcb3181b4b1b9f5577e0687b871a0207b862
chansjeo <kapustin4613@gmail.com>
2024-11-08 10:08:48 +0000

    ajajajajaajja

--- inception/srcs/requirements/mariadb/tools/init_sql.sh
+++ inception/srcs/requirements/mariadb/tools/init_sql.sh
@@ -3,9 +3,16 @@
 rc-service mariadb setup
 rc-service mariadb start
 
-mysql -e "CREATE USER 'root'@'%' IDENTIFIED BY 'password';" && \
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" && \
-mysql -e "FLUSH PRIVILEGES;" && \
+mysql -u $(MYSQL_ROOT_USER) <<EOF
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO '$(MYSQL_ROOT_USER)'@'%' identified by '$(MYSQL_ROOT_PASSWORD)' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO '$(MYSQL_ROOT_USER)'@'localhost' identified by '$(MYSQL_ROOT_PASSWORD)' WITH GRANT OPTION ;
+SET PASSWORD FOR '$(MYSQL_ROOT_USER)'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+EOF
+
 mysql_secure_installation <<EOF
 y
 password
@@ -17,9 +24,10 @@ y
 EOF
 
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|^skip-networking|#skip-networking|g" /etc/mysql/my.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf
 sed -i "s|^skip-networking|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 
 rc-service mariadb restart
-rc-update add mariadb default
 rc-service mariadb stop
+
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@

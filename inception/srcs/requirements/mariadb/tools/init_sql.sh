9c67a328bfb836ed5910d572d35fa9e6a1299ca0
chansjeo <kapustin4613@gmail.com>
2024-11-08 13:41:25 +0000

    asdf

--- inception/srcs/requirements/mariadb/tools/init_sql.sh
+++ inception/srcs/requirements/mariadb/tools/init_sql.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
-rc-service mariadb setup
-rc-service mariadb start
+if [ -d "/run/mysqld" ]; then
+    echo "[i] mysqld already present, skipping creation"
+    chown -R mysql:mysql /run/mysqld
+else
+    echo "[i] mysqld not found, creating..."
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
 
-mysql -u $(MYSQL_ROOT_USER) <<EOF
+if [ -d /var/lib/mysql/mysql ]; then
+    echo "[i] MySQL directory already present, skipping creation"
+    chown -R mysql:mysql /var/lib/mysql
+else    
+    echo "[i] MySQL data directory not found, creating initial DBs"
+
+    chown -R mysql:mysql /var/lib/mysql
+
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+
+    tfile=$(mktemp)
+    mysql -u $MYSQL_ROOT_USER <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL ON *.* TO '$(MYSQL_ROOT_USER)'@'%' identified by '$(MYSQL_ROOT_PASSWORD)' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO '$(MYSQL_ROOT_USER)'@'localhost' identified by '$(MYSQL_ROOT_PASSWORD)' WITH GRANT OPTION ;
-SET PASSWORD FOR '$(MYSQL_ROOT_USER)'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+GRANT ALL ON *.* TO '$MYSQL_ROOT_USER'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO '$MYSQL_ROOT_USER'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR '$MYSQL_ROOT_USER'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 EOF
@@ -23,11 +40,9 @@ y
 y
 EOF
 
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf
-sed -i "s|^skip-networking|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+fi
 
-rc-service mariadb restart
-rc-service mariadb stop
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|^skip-networking|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 
 exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@

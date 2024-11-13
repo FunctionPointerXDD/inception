70299df5dd5a75c417a150540edd089a825162dd
chansjeo <kapustin4613@gmail.com>
2024-11-13 08:08:07 +0000

    asdfasdf

--- inception/srcs/requirements/wordpress/conf/wp-config.php
+++ inception/srcs/requirements/wordpress/conf/wp-config.php
@@ -12,3 +12,11 @@ define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
 
 /** Database hostname */
 define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';

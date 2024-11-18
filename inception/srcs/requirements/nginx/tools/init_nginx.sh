062a96a38c0b4d767669d5af4f331bd87966bf3e
chansjeo <kapustin4613@gmail.com>
2024-11-18 09:44:18 +0000

    asdf

--- inception/srcs/requirements/nginx/tools/init_nginx.sh
+++ inception/srcs/requirements/nginx/tools/init_nginx.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 #create ssl certification
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${CERTS_KEY_PATH} \
-        -out ${CERTS_PATH} \
-        -subj "/C=kr/ST=seoul/L=gaepo/O=chansjeo/CN=chans_cert"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${CERTS_KEY_PATH} -out ${CERTS_PATH}
+#        -subj "/C=kr/ST=seoul/L=gaepo/O=chansjeo/CN=chans_cert"
 
 chmod 600 ${CERTS_KEY_PATH} ${CERTS_PATH}
 

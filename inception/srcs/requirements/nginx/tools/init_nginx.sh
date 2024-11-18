9358bca10cc9448a36aa421837ed821e5f71e755
chansjeo <kapustin4613@gmail.com>
2024-11-18 08:25:06 +0000

    asdfasdf

--- inception/srcs/requirements/nginx/tools/init_nginx.sh
+++ inception/srcs/requirements/nginx/tools/init_nginx.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 #create ssl certification
-openssl req
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${CERTS_KEY_PATH} \
+        -out ${CERTS_PATH} \
+        -subj "/C=kr/ST=seoul/L=gaepo/O=chansjeo/CN=chans_cert"
 
-exec nginx
+chmod 600 ${CERTS_KEY_PATH} ${CERTS_PATH}
+
+sed -i "s|DOMAIN_NAME|${DOMAIN_NAME}|g" /etc/nginx/nginx.conf
+sed -i "s|CERTS_PATH|${CERTS_PATH}|g" /etc/nginx/nginx.conf
+sed -i "s|CERTS_KEY_PATH|${CERTS_KEY_PATH}|g" /etc/nginx/nginx.conf
+ 
+exec nginx -g "daemon off;"
